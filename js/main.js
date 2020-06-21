477ffe9b51901cde32bc14a9cbb038482c84bd2d
Mojo <47996003+1MaddieX@users.noreply.github.com>
2020-06-20 20:26:33 -0400

    Updated Website

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Main JS Doc for PEG
+//I miss nodejs so bad :( ...
+//clickhide func
+function clickHide(id) {
+  var ele = document.getElementById(id);
+  //wait a bit
+  //document.getElementById("Down").style.animation = "animation: flyAway 1s ease-in-out 0s 1 forwards";
+  setTimeout(() => {ele.style.display = "none";}, 1000);
+}
+
+
+
